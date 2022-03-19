c9ec1f310355c7c40d35df3cfb58f03cd25fd6b7
serciak <jakubseredynski@gmail.com>
2022-03-19 23:42:38 +0100

    linked list

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,7 +1,60 @@
 package main;
 
-public class Main {
-    public static void main(String[] args) {
+import java.io.*;
+import java.util.Iterator;
 
+public class Main {
+
+    public static void removeWithKey(OneWayLinkedList<Student> s, double grade) {
+        Iterator<Student> it = s.iterator();
+        Student temp;
+
+        while(it.hasNext()) {
+            temp = it.next();
+            if(temp.getGrade() < grade) {
+                s.remove(temp);
+            }
+        }
+    }
+
+    public static void serialize(OneWayLinkedList<Student> s) {
+        try {
+            ObjectOutputStream oOS = new ObjectOutputStream(new FileOutputStream("students.ser"));
+
+            oOS.writeObject(s);
+            oOS.close();
+
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static OneWayLinkedList<Student> deserialize() {
+        try {
+            ObjectInputStream oIS = new ObjectInputStream(new FileInputStream("students.ser"));
+
+            OneWayLinkedList<Student> s = (OneWayLinkedList<Student>) oIS.readObject();
+            oIS.close();
+
+            return s;
+        } catch(IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+        OneWayLinkedList<Student> students = new OneWayLinkedList<>();
+
+        students.add(new Student(1, "Jakub", "Seredynski", 3.0));
+        students.add(new Student(3, "Jakub", "Lewy", 5.5));
+        students.add(new Student(10, "Jakub", "Jelen", 4.0));
+
+        serialize(students);
+        removeWithKey(students, 5);
+        System.out.println(students);
+
+        students = deserialize();
+        System.out.println(students);
     }
 }
